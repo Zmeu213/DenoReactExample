ebf3660d1f102abbef6ccc06499259f3403dbf44
Кирилл <opoxmeloid@yandex.ru>
2020-05-18 17:56:57 +0300

    fix typo

--- util/usePage.js
+++ util/usePage.js
@@ -1,6 +1,6 @@
 import ReactDOMServer from 'https://dev.jspm.io/react-dom/server'
 import React from 'https://dev.jspm.io/react'
-import Head from './components/Head.jsx'
+import Head from '../components/Head.jsx'
 
 export default function usePage(page) {
   return ctx => {
